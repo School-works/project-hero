0db46a832cdf3178f5c57227707596d83a124164
bad0dab <broadnicegamerr@gmail.com>
2026-04-14 09:28:55 +0200

    models and components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroCardComponent } from './components/hero.card.component/hero.card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroCardComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

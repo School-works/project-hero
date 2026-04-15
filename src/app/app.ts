fff03e38cf2d1241f6475f19fc332408a09cb76b
bad0dab <broadnicegamerr@gmail.com>
2026-04-15 10:22:42 +0200

    restarting project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeroCardComponent } from './components/hero.card.component/hero.card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/hero.card.component/hero.card.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-hero-card',
-  imports: [],
-  templateUrl: './hero.card.component.html',
-  styleUrl: './hero.card.component.css',
-})
-export class HeroCardComponent {}
